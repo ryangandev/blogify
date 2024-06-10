1a7de459d4de65d49d78f07c87aab39d9cdd9571
Ryan Gan <ganzhiheng1@gmail.com>
2024-06-09 22:58:44 -0400

    Use rate limiter middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import rateLimit from 'express-rate-limit';
 
 import { logger } from './middleware/loggerMiddleware';
 import { errorHandler } from './middleware/errorHandlerMiddleware';
@@ -13,6 +14,14 @@ import tagRoutes from './routes/tagRoutes';
 import userRoutes from './routes/userRoutes';
 import adminRoutes from './routes/adminRoutes';
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    message: 'Too many requests from this IP, please try again later',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 
 // Middleware
@@ -21,6 +30,7 @@ app.use(json({ limit: '1kb' }));
 app.use(cookieParser()); // Parse cookies
 app.use(logger); // Log requests
 app.use(errorHandler); // Handle errors
+app.use(limiter); // Rate limit requests
 
 // Serve static files from the public folder
 app.use(express.static(path.join(__dirname, '..', 'public')));
